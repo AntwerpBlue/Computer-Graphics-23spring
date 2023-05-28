a0eeae626bb42acca6454f9df7ff094aac82cf51
Oren <112540659+AntwerpBlue@users.noreply.github.com>
2023-05-28 20:41:47 +0800

    Delete Homework02 directory

--- Homework02/toolPositionCB.m
+++
@@ -1,8 +0,0 @@
-function toolPositionCB(h, varargin)
-
-%   Copyright © 2021, Renjie Chen @ USTC
-
-set(h, 'Enable', 'off');
-
-subplot(121);
-set(h, 'Enable', 'on', 'UserData', [h.UserData, imline]);
